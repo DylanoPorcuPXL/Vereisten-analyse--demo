bd66b7ee8f39ee35f9e065d953ccee6b69920220
Dylano Porcu <dylano.porcu@student.pxl.be>
2025-11-06 13:33:05 +0100

    analyse demo

---
+++ analyse demo/analyse demo/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace analyse_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string user = "Dylano", password = "PXL123";
+            int tries = 0;
+
+            while (tries < 3)
+            {
+                Console.Write ("Gebruikersnaam: ");
+                string inputuser = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                string inputPassword = Console.ReadLine(); 
+
+                if (inputuser == user && inputPassword == password)
+                {
+                    Console.WriteLine($"Welkom, {inputuser}");
+                    return;
+                }
+                tries++;
+                Console.WriteLine($"Foutive login.\n {3 - tries} pogingen over");
+            }
+            Console.WriteLine("te veel pogingen. geen toegang");
+        }
+    }
+}
